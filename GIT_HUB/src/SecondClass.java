f4bb7f2f1c3343f9edc279f90a0fe22d427f6064
kamma nageswararao <kamma.nageswararao@gmail.com>
2018-10-03 18:21:16 +0530

    SecondClass comitted.
    Signed-off-by: kamma nageswararao <kamma.nageswararao@gmail.com>

---
+++ GIT_HUB/src/SecondClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author knageswarar2
+ *
+ */
+public class SecondClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is the Second Class");
+		
+	}
+
+}
