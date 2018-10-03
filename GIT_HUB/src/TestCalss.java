f42cd70aad84e56812f181af2c1764dc53f35b85
kamma nageswararao <kamma.nageswararao@gmail.com>
2018-10-03 17:46:22 +0530

    Added TestClass.java

---
+++ GIT_HUB/src/TestCalss.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author knageswarar2
+ *
+ */
+public class TestCalss {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("WELCOME TO GIT LEARNER !!!!!!");
+	}
+
+}
