ffb029c0e0f6d548c1ffd5a2da7c0643a7613e8c
kamma nageswararao <kamma.nageswararao@gmail.com>
2018-10-04 12:45:21 +0530

    Testerclass added

---
+++ GIT/src/Testerclass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author knageswarar2
+ *
+ */
+public class Testerclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("pRINTED");
+		
+	}
+
+}
